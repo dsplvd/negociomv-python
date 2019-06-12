13eb5eac3d6c66858195620794b3185e4e9b345e
dsplvd <d@splvd.com>
2019-06-12 18:05:46 -0400

    Watchdog operational

--- watchdog.py
+++ watchdog.py
@@ -1,12 +1,32 @@
 import pyinotify
+import subprocess
+import sys
+import time
+import threading
 
 class ModHandler(pyinotify.ProcessEvent):
+    count = 0
+
+    def _restart_timer(self):
+        print('==> RUN ENDED')
+	self.count = 0
+
+    def _run_cmd(self):
+        print('==> RUNNING')
+	#print('==> RUN ENDED')
+        timer = threading.Timer(30, self._restart_timer)
+        timer.start()
+        #subprocess.call(self.cmd.split(' '), cwd=self.cwd)
+
     # evt has useful properties, including pathname
-    def process_IN_CLOSE_WRITE(self, evt):
-            pprint('Data changed')
+    def process_IN_MODIFY(self, evt):
+        print('Data changed')
+        if self.count < 1:
+            self._run_cmd()
+	self.count +=1
 
 handler = ModHandler()
 wm = pyinotify.WatchManager()
 notifier = pyinotify.Notifier(wm, handler)
-wdd = wm.add_watch('README.md', pyinotify.IN_CLOSE_WRITE)
+wdd = wm.add_watch('/home/pi/piusb.bin', pyinotify.IN_MODIFY)
 notifier.loop()
