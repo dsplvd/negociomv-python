46c1287635f024347790f90741b098468448884e
dsplvd <d@splvd.com>
2019-06-25 15:26:07 -0400

    Balena fixes

--- start.sh
+++ start.sh
@@ -1,2 +1,14 @@
 #/bin/bash
-python solid.py
+
+FILE=/data/piusb.bin
+if test -f "$FILE"; then
+    echo "$FILE exist"
+else
+  dd if=/dev/zero of=/data/piusb.bin bs=1M count=128
+  mkdosfs /data/piusb.bin
+  mkdir /data/temp_files/
+fi
+
+modprobe g_mass_storage file=/data/piusb.bin removable=1 ro=0 stall=0
+
+python gpio.py && pon huawei && python solid-balena.py
