b4375612499fd0a51fc9d3f36bc6f9ce7578d323
dsplvd <d@splvd.com>
2019-06-25 13:32:11 -0400

    Gpio

---
+++ gpio.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+   
+import RPi.GPIO as GPIO
+import time
+   
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+   
+GPIO.output(26, True)
+time.sleep(2)
+GPIO.output(26, False)
+   
+GPIO.cleanup()
