cc897c999dcf54b40adf91ad92a9e650b105b363
dsplvd <d@splvd.com>
2020-01-07 17:41:43 -0300

    Update .gitignore

--- creds.py
+++ creds.py
@@ -30,3 +30,16 @@ if not creds or not creds.valid:
     # Save the credentials for the next run
     with open('token.pickle', 'wb') as token:
         pickle.dump(creds, token)
+
+service = build('drive', 'v3', credentials=creds)
+
+results = service.files().list(
+    pageSize=10, fields="nextPageToken, files(id, name)").execute()
+items = results.get('files', [])
+
+if not items:
+    print('No files found.')
+else:
+    print('Files:')
+    for item in items:
+        print(u'{0} ({1})'.format(item['name'], item['id']))
